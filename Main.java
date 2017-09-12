088fc502a1a7f1ab0362c19839ab9b2d9a76e179
dadakhalandar22 <31854620+dadakhalandar22@users.noreply.github.com>
2017-09-12 12:44:03 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -2,7 +2,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		//You may test that your code works find here
+		Student();
+		StudentArrayOperation();
+		StudentGroup();
+//You may test that your code works find here
 		//Please check that your code works and has no 
 		//compilation problems before to submit
 	}
